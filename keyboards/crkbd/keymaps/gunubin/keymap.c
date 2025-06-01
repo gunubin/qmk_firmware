d580671880a13383e811936085df274034e1eeb5
Koki Kato <kato@shanord.com>
2025-06-01 13:52:49 +0900

    👍 feat: 順次コンボ入力とカスタムキーマップを追加
    - 順次コンボロジック(`sequential_combo.c/h`)を追加し、複雑なキー入力を簡略化
    - `crkbd`用のカスタムキーマップを作成し、動作を最適化
    - `rules.mk`を更新し、新しいファイルをコンパイル対象に設定
    - `config.h`にタイムアウトや入力設定を追加し、柔軟性を向上
    - 特定のキーやレイヤーでの直感的な動作を実現し、ユーザー体験を改善

---
+++ keyboards/crkbd/keymaps/gunubin/keymap.c
@@ -0,0 +1,173 @@
+/* Copyright 2020 Paul James
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "sequential_combo.h"
+
+enum layers {
+  _QWERTY = 0,
+  _FUNCTION,
+  _NUMBER
+};
+
+#define A_SPC LGUI_T(KC_SPC)
+#define G_TAB LALT_T(KC_TAB)
+
+#define A_BSPC RALT_T(KC_BSPC)
+#define L1_BSPC LT(1, KC_BSPC)
+
+#define L2_DEL LT(2, KC_DEL)
+
+#define C_A LCTL_T(KC_A)
+#define C_ENT LCTL_T(KC_ENT)
+
+#define S_Z LSFT_T(KC_Z)
+#define S_SLSH LSFT_T(KC_SLSH)
+
+const uint16_t PROGMEM sleep_combo[] = {KC_F14, KC_F15, COMBO_END};
+// const uint16_t PROGMEM one_shot_shift_combo[] = {KC_F, KC_J, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(sleep_combo, KC_NO), // combo index 0
+//     COMBO(one_shot_shift_combo, OSM(MOD_LSFT)),
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch(combo_index) {
+        case 0:
+            if (pressed) {
+                // option + cmd + backspace (system sleep)
+                register_code16(RALT(KC_LGUI));
+                tap_code(KC_BSPC);
+                unregister_code16(RALT(KC_LGUI));
+            }
+            break;
+    }
+}
+
+static const sequential_combo_entry_t my_combos[] = {
+    {LSFT(KC_9), LSFT(KC_9), LSFT(KC_0)}, // double tap "(" to ")"
+    {KC_LCBR, KC_LCBR, KC_RCBR}, // double tap "{" to "}"
+    {KC_LBRC, KC_LBRC, KC_RBRC}, // double tap "[" to "]"
+    {LSFT(KC_COMM), LSFT(KC_COMM), LSFT(KC_DOT)}, // double tap "<" to ">"
+    {KC_DOT, KC_COMM, KC_SCLN}, // tap "." "," to ";"
+    {KC_COMM, KC_DOT, KC_EQL}, // tap "," "." to "="
+    {KC_COMM, KC_COMM, LSFT(KC_SCLN)}, // double tap "," to ":"
+    {S_Z, S_SLSH, LSFT(KC_SLSH)}, // tap "z" "/" to "?"
+    {KC_QUOT, KC_QUOT, KC_DQT}, // tap "z" "/" to "?"
+};
+
+void keyboard_post_init_user(void) {
+    init_sequential_combos(my_combos, sizeof(my_combos) / sizeof(my_combos[0]));
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_QWERTY] = LAYOUT_split_3x6_3(
+      _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+      _______, C_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    C_ENT,   _______,
+      _______, S_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  S_SLSH,  _______,
+                         G_TAB,   A_SPC,   KC_SPC,  KC_RSFT, L1_BSPC, L2_DEL
+    ),
+
+    [_FUNCTION] = LAYOUT_split_3x6_3(
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  KC_TILD, _______,
+      _______, KC_PLUS, LSFT(KC_9), KC_LBRC, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_SCLN, _______,
+      _______, KC_DQT,  KC_LCBR, LSFT(KC_COMM), KC_QUOT, KC_UNDS, KC_PIPE, KC_UNDS, LSFT(KC_COMM), LSFT(KC_DOT), KC_BSLS, _______,
+                         KC_LGUI, KC_LALT, _______, _______, KC_SPC,  _______
+    ),
+
+    [_NUMBER] = LAYOUT_split_3x6_3(
+      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                         KC_LGUI, KC_LALT, _______, _______, KC_SPC,  _______
+    )
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    if (!process_sequential_combo_keys(keycode, record)) return false;
+
+    return true;
+}
+
+void housekeeping_task_user(void) {
+    sequential_combo_housekeeping();
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case S_Z:
+            return 155;
+        case C_A:
+//             return 170;
+            return 180;
+        case C_ENT:
+            return 180;
+        default:
+            return 200;
+    }
+}
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case A_BSPC:
+        case L1_BSPC:
+            return 0;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case A_SPC:
+        case G_TAB:
+        case A_BSPC:
+        case L1_BSPC:
+        case S_SLSH:
+            return false;
+        default:
+           return PERMISSIVE_HOLD;
+    }
+}
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case A_SPC:
+        case G_TAB:
+        case A_BSPC:
+        case L1_BSPC:
+        case S_SLSH:
+            return true;
+        default:
+            return HOLD_ON_OTHER_KEY_PRESS;
+    }
+}
+
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case A_SPC:
+        case G_TAB:
+        case A_BSPC:
+        case L1_BSPC:
+        case S_SLSH:
+            return false;
+        default:
+            return TAPPING_FORCE_HOLD;
+    }
+}
